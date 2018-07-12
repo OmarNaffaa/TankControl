a49041eb8477a3641d78f0735c8622133c783a62
Omar Naffaa <omarnaffaa.on@gmail.com>
2018-07-11 17:47:46 -0700

    The power comsumption section now displays the current and voltage when the "View Details" button is pushed

---
+++ app/src/main/java/com/example/naffaa/tankcontrol/PowerActivity.java
@@ -0,0 +1,16 @@
+package com.example.naffaa.tankcontrol;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PowerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_power);
+        getSupportActionBar().hide();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
