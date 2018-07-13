9d4e575788eca8e87cb50e08447d71f889821c11
Omar Naffaa <omarnaffaa.on@gmail.com>
2018-07-13 12:59:58 -0700

    Successfully retrieved a field from ThingSpeak, attempting to create a class that handles getting data

--- app/src/main/java/com/example/naffaa/tankcontrol/MainActivity.java
+++ app/src/main/java/com/example/naffaa/tankcontrol/MainActivity.java
@@ -8,7 +8,18 @@ import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        TextView temp = findViewById(R.id.temp_val);
+        getData(temp);
+
         ToggleButton toggleMotor = findViewById(R.id.motorToggle);
         ToggleButton toggleValve = findViewById(R.id.valveToggle);
         Button powerButton = findViewById(R.id.powerButton);
@@ -26,8 +40,45 @@ public class MainActivity extends AppCompatActivity {
 
     // Receives the data from ThingSpeak and displays it on the appropriate
     // textbox
+    String server_url =
+            "https://api.thingspeak.com/channels/525549/feeds.json?api_key=7I4UJ8MNLR8I0LWS&results=1";
 
+    private void getData(final TextView temperature) {
 
+        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, server_url, (JSONObject) null,
+        new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try
+                {
+                    JSONArray outer = response.getJSONArray("feeds");
+                    JSONObject inner = outer.getJSONObject(0);
+
+                    // gets each field from ThingSpeak
+                    String temp = inner.getString("field2");
+                    String pow = inner.getString("field1");
+
+                    // set the field to the appropriate textbox
+                    temperature.setText(temp + " V");
+
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+
+                Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        MySingleton.getInstance(MainActivity.this).addToRequestQueue(objectRequest);
+
+    }
 
     // event listener for this method is controlled by the powerButton
     // in the XML sheet
